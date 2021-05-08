0b5c32e3becc75881ece760a715699ed2cec9ed8
Rina Trofimova <lidesrose@gmail.com>
2021-05-09 03:23:15 +0700

    fix

--- Lab9/Form1.Designer.cs
+++ Lab9/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace Lab9
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.prob1 = new System.Windows.Forms.NumericUpDown();
             this.prob2 = new System.Windows.Forms.NumericUpDown();
@@ -57,16 +57,16 @@ namespace Lab9
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(15, 15);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(491, 319);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -131,7 +131,7 @@ namespace Lab9
             this.prob5.Size = new System.Drawing.Size(106, 22);
             this.prob5.TabIndex = 5;
             this.prob5.Value = new decimal(new int[] {
-            1,
+            2,
             0,
             0,
             65536});
